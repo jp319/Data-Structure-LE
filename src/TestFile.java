4e9c86b1f3bb8d69bfaeb5070fcd75a2280f0581
Vanillacakes26 <vlfvillanueva00367@usep.edu.ph>
2022-12-15 15:14:08 +0800

    MAO NI ANG BAG-O NA MESSAGE

--- src/TestFile.java
+++ src/TestFile.java
@@ -1,7 +1,7 @@
 public class TestFile {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Si vim jud diay ni HAHAHAHAHa");
+        System.out.println("NAKACONNECT NAKO!");
 
     }
 }
